4312f0afc515385a4234648a5a8c68689e6afaa6
isurudilshan101 <isuru.dilshan101@gmail.com>
2022-02-05 00:00:09 +0530

    review

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.84.0">
-    <title>Blog App</title>
+    <title>Blog App with CRUD</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/blog/">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
